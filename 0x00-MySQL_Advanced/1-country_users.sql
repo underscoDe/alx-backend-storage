51913f110216b421c0fd7c406290eacadf60d716
Arsene Awounou <developer.awounou@gmail.com>
2022-07-02 18:29:31 +0100

    constraint

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    country VARCHAR(2) NOT NULL DEFAULT 'US',
+    CONSTRAINT chk_country CHECK (country IN ('US', 'CO', 'TN'))
 );
