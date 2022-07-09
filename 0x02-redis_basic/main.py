7259d8728212523098c0ac34ce3147be7f5a03ea
Arsene Awounou <developer.awounou@gmail.com>
2022-07-09 13:31:24 +0100

    update main

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+""" Main file """
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
 
-cache = Cache()
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
