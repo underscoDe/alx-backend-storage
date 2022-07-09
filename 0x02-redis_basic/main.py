ecbbc9b35c73f57fdfe7c6f9b339e711c41020c8
Arsene Awounou <developer.awounou@gmail.com>
2022-07-09 01:22:18 +0100

    updates tests

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -14,3 +14,14 @@ print(key)
 
 local_redis = redis.Redis()
 print(local_redis.get(key))
+
+cache = Cache()
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
