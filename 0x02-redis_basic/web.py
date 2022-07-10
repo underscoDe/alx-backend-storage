15bf80652564c405dba952655ccc1e8f7ef041f9
Arsene Awounou <developer.awounou@gmail.com>
2022-07-10 20:33:05 +0100

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Implements an expiring web cache and tracker
+"""
+from typing import Callable
+from functools import wraps
+import redis
+import requests
+redis_client = redis.Redis()
+count = 0
+
+
+def url_count(method: Callable) -> Callable:
+    """decorator for url count"""
+    @wraps(method)
+    def wrapper(*args):
+        redis_client.incr(f"count:{args[0]}")
+        return method(*args)
+    return wrapper
+
+def get_page(url: str) -> str:
+    """ get a page and cache value"""
+    redis_client.set(f'cached:{url}', count)
+    response = requests.get(url)
+    redis_client.setex(f'cached:{url}', 10, redis_client.get(f'cached:{url}'))
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
